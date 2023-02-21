ff2c9d6b1f5205fa1e8037f60e09622aa566fa6c
Andrii Sorkand <96412234+sorkand1@users.noreply.github.com>
2023-02-21 13:01:54 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function logo {
+  bash <(curl -s https://raw.githubusercontent.com/sorkand1/nodes/main/welcome.sh)
+}
+
+function line {
+  echo "-----------------------------------------------------------------------------"
+}
+
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
+
+function docker_down {
+  docker-compose down
+}
+
+colors
+line
+logo
+line
+docker_down
+line
