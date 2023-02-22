26878e908233d3513001b42c1b9f702b88436272
Andrii Sorkand <96412234+sorkand1@users.noreply.github.com>
2023-02-22 09:10:49 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,42 +1,13 @@
 #!/bin/bash
 
-function logo {
-  bash <(curl -s https://raw.githubusercontent.com/sorkand1/nodes/main/welcome.sh)
+function source {
+  source ~/.profile
 }
 
-function line {
-  echo "-----------------------------------------------------------------------------"
-}
-
-function colors {
-  GREEN="\e[1m\e[32m"
-  RED="\e[1m\e[39m"
-  NORMAL="\e[0m"
-}
-
-function docker_down {
-  docker-compose down
-}
-
-function docker_pull {
-  docker-compose pull
-}
-
-function docker_run {
-  docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish
-}
-
-function docker_up {
-  docker-compose up -d
+function status {
+  ironfish status
 }
 
 
-colors
-line
-logo
-line
-docker_down
-docker_pull
-docker_run
-docker_up
-line
+source
+status
